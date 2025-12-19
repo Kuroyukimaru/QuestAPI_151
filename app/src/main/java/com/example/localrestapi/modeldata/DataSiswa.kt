e7f73063f88d624efacbf742d4ae43ccc79f5cbc
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-12-19 10:26:58 +0700

    menyimpan data input/form UI

--- app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
@@ -10,3 +10,15 @@ data class DataSiswa(
     val telpon : String
 )
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
