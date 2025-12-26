4794402a543dca84829d93fcab56fbe9d8e152d2
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-12-26 13:51:56 +0700

    Menghapus data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -22,7 +22,7 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
-class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
@@ -31,17 +31,29 @@ class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositor
         getSatuSiswa()
     }
 
-    fun getSatuSiswa() {
+    fun getSatuSiswa(){
         viewModelScope.launch {
             statusUIDetail = StatusUIDetail.Loading
             statusUIDetail = try {
                 StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
-            } catch (e: IOException) {
+            }
+            catch (e: IOException){
                 StatusUIDetail.Error
-            } catch (e: HttpException) {
+            }
+            catch (e: HttpException){
                 StatusUIDetail.Error
             }
         }
     }
 
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa(){
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if(resp.isSuccessful){
+            println("Sukses Hapus Data : ${resp.message()}")
+        }else{
+            println("Gagal Hapus Data : ${resp.errorBody()}")
+        }
+    }
 }
