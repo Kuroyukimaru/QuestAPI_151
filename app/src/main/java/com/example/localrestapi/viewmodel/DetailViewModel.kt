205aca6f0b03f9ac24c29f5caf1df83921d47444
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-12-26 13:51:42 +0700

    Mengambil 1 data siswa dari API

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -22,7 +22,7 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
-class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
@@ -31,5 +31,17 @@ class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositor
         getSatuSiswa()
     }
 
-    
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
 }
